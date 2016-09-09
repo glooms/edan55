1f33995d7393d106cbf9b3467d465d453a2cc8e1
Glooms <dat11mla@student.lu.se>
2016-09-09 11:58:48 +0200

    Added everything for a decent implemenation of lab2.

---
+++ lab2/beauty.py
@@ -0,0 +1,17 @@
+import io
+import sys
+from random import randint
+
+f = open(sys.argv[1], 'r')
+n = int(f.readline().split(' ')[0])
+nodes = [0] * (n + 1)
+maxcut = 0
+for line in f :
+    t = [int(i) for i in line.split()]
+    if nodes[t[0]] == 0 :
+        nodes[t[0]] = randint(1, 2)
+    if nodes[t[1]] == 0 :
+        nodes[t[1]] = randint(1, 2)
+    if nodes[t[0]] != nodes[t[1]] :
+        maxcut += t[2]
+print maxcut
