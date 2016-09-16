303f426002ede6a7b573fc23718db2b9a27ca568
Glooms <dat11mla@student.lu.se>
2016-09-16 10:15:37 +0200

    Removed an unnecessary whitespace

--- lab2/beauty.py
+++ lab2/beauty.py
@@ -8,7 +8,7 @@ maxcut = 0
 for line in f :
     t = [int(i) for i in line.split()]
     if nodes[t[0]] == 0: nodes[t[0]] = randint(1, 2)
-    if nodes[t[1]] == 0:  nodes[t[1]] = randint(1, 2)
+    if nodes[t[1]] == 0: nodes[t[1]] = randint(1, 2)
     if nodes[t[0]] != nodes[t[1]]: maxcut += t[2]
 print maxcut
 
