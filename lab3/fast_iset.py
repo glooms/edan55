2bdd81e592e436b83af6af2849d68a77f7cc12db
Glooms <dat11mla@student.lu.se>
2016-09-22 02:27:56 +0200

    The fast version works, half the time, uses gmpy though...

--- lab3/fast_iset.py
+++ lab3/fast_iset.py
@@ -1,33 +1,39 @@
 import sys
+import gmpy
+import math
 
 def remove(v, V, mask):
     V.remove(v)
-    mask = mask - 1<<(n - 1 - v)
-    return (V, mask)
-
-def p(V, mask):
-    print V
-    print bin(mask)[2:].zfill(n)
+#    mask = mask - 1<<(n - 1 - v)
+    mask -= 1<<(n - 1 - v)
+    return mask
 
 def r0(V, mask):
+    global call_count
+    call_count += 1
     if not V:
         return 0
     u = -1
     max_deg = 0
     for v in V:
-        s = bin(G[v] & mask).count('1')
-        if s == 0:
-            (V, mask) = remove(v, V, mask)
+        s = gmpy.popcount(G[v] & mask)
+        if s == 0: 
+            mask = remove(v, V, mask)
+            return 1 + r0(V, mask)
+        if s == 1: #R1
+            mask = remove(v, V, mask)
+            nb = int(round(n - 1 - math.log(G[v] & mask, 2)))
+            mask = remove(nb, V, mask)
             return 1 + r0(V, mask)
         if s > max_deg:
             u = v
             max_deg = s
-    (V, mask) = remove(u, V, mask)
+    mask = remove(u, V, mask)
     a = r0(list(V), mask)
     V1 = list(V)
     for nb in V:
-        if G[u] & 1<<(n - 1 - nb) != 0:
-            (V1, mask) = remove(nb, V1, mask)
+        if G[u] & 1<<(n - 1 - nb):
+            mask = remove(nb, V1, mask)
     return max(1 + r0(V1, mask), a)
 
 
@@ -38,8 +44,10 @@ G = [int(line.strip().replace(' ',''), base=2) for line in f]
 V = range(n)
 mask = 2**n - 1
 
-print n
-print V
-print mask
+#print n
+#print V
+#print mask
 
+call_count = 0
 print r0(V, mask)
+print call_count
